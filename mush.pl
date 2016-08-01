bd1c0a93d710edaf9977f6809ed74549c211f602
Dave Lambley <dave@lambley.me.uk>
2016-08-01 22:43:29 +0100

    Works, apart from slow down.

--- mush.pl
+++ mush.pl
@@ -13,15 +13,20 @@ my $res = "640x360";
 
 my $ffmpeg = fork() // die $!;
 
+my $fps = 30;
+my $chunk_duration = 30000;
+my $keyint_min = $chunk_duration/$fps;
+
+
 if ($ffmpeg==0) {
 	my $vp9="-speed 6 -tile-columns 4 -frame-parallel 1 -threads 3 -static-thresh 0 -max-intra-rate 300 -deadline realtime -lag-in-frames 0 -error-resilient 1";
-	exec(qq{ffmpeg -f v4l2 -input_format mjpeg -r 30 -s $res -thread_queue_size 1024 -i /dev/video0 }.
-             qq{-f alsa -ar 48000 -ac 1 -thread_queue_size 1024 -i pulse }.
+	exec(qq{ffmpeg -f v4l2 -input_format mjpeg -r $fps -s $res -thread_queue_size 2048 -i /dev/video0 }.
+             qq{-f alsa -ar 48000 -ac 1 -thread_queue_size 2048 -i pulse }.
              qq{-map 0:0 }.
              qq{-pix_fmt yuv420p }.
              qq{-c:v libvpx-vp9 }.
-             qq{-s $res -keyint_min 60 -g 60 $vp9 }.
-             qq{-b:v 1000k }.
+             qq{-s $res -keyint_min $keyint_min -g $keyint_min $vp9 }.
+             qq{-b:v 128k }.
              qq{-aspect 16:9 }.
              qq{-f webm_chunk }.
              qq{-header "${prefix}glass_360.hdr" }.
@@ -31,7 +36,7 @@ if ($ffmpeg==0) {
              qq{-c:a libvorbis }.
              qq{-b:a 64k -ar 48000 }.
              qq{-f webm_chunk }.
-             qq{-audio_chunk_duration 2000 }.
+             qq{-audio_chunk_duration $chunk_duration }.
              qq{-header ${prefix}glass_171.hdr }.
              qq{-chunk_start_index 1 }.
              qq{${prefix}glass_171_%d.chk }
@@ -55,10 +60,10 @@ system(qq{ffmpeg -y -f webm_dash_manifest -live 1 }.
        qq{-f webm_dash_manifest -live 1 }.
        qq{-adaptation_sets "id=0,streams=0 id=1,streams=1" }.
        qq{-chunk_start_index 1 }.
-       qq{-chunk_duration_ms 2000 }.
+       qq{-chunk_duration_ms $chunk_duration }.
        qq{-time_shift_buffer_depth 7200 }.
        qq{-minimum_update_period 7200 }.
-       qq{-utc_timing_url "https://linuxbierwanderung.com/live/${pre}now.txt" }.
+       qq{-utc_timing_url "https://linuxbierwanderung.com/live/${pre}now.txt?iso" }.
        qq{${prefix}glass_live_manifest.mpd }
 )==0 or die;
 
@@ -87,7 +92,7 @@ close $fh;
 while (1) {
 	open(my $fh, ">", "/var/www/html/live/${pre}now.txt.new") or die $!;
 	sleep 1-(time-int(time));
-	print $fh DateTime->now->add( seconds => -6 )->iso8601."Z\n";
+	print $fh DateTime->now->add( seconds => -60 )->iso8601."Z";
 	close $fh;
 	rename("/var/www/html/live/${pre}now.txt.new", "/var/www/html/live/${pre}now.txt") or die $!;
 }
